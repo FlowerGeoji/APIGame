b026b8c0df83b63ed90ed11ce9ce0dcb0b987bd2
Flowergeoji <gyujae.kim@insertcoin.me>
2018-10-04 11:02:55 +0900

    init ios

---
+++ APIGameIOS/APIGameIOS/APIGameIOS.h
@@ -0,0 +1,19 @@
+//
+//  APIGameIOS.h
+//  APIGameIOS
+//
+//  Created by FlowerGeoji on 2018. 10. 4..
+//  Copyright © 2018년 FlowerGeoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIGameIOS.
+FOUNDATION_EXPORT double APIGameIOSVersionNumber;
+
+//! Project version string for APIGameIOS.
+FOUNDATION_EXPORT const unsigned char APIGameIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIGameIOS/PublicHeader.h>
+
+
