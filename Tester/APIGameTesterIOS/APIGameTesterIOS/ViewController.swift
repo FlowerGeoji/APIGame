51a92e663df2b02a93697fce79439f1afb3ff105
Flowergeoji <gyujae.kim@insertcoin.me>
2018-10-04 18:20:59 +0900

    Add ios tester project

---
+++ Tester/APIGameTesterIOS/APIGameTesterIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIGameTesterIOS
+//
+//  Created by FlowerGeoji on 2018. 10. 4..
+//  Copyright © 2018년 FlowerGeoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
