86d11715ad00bff5cf7b8024d98fc1910b90a60c
Flowergeoji <gyujae.kim@insertcoin.me>
2018-11-12 11:27:44 +0900

    ios

---
+++ Tester/TestiOSProject/TestiOSProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestiOSProject
+//
+//  Created by FlowerGeoji on 08/11/2018.
+//  Copyright © 2018 FlowerGeoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
